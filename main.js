bc11204be1bbeb726d633b53d19360a91fc4dc70
Ayushman Bilas Thakur <ayushmanbilasthakur@gmail.com>
2018-11-21 03:35:34 +0530

    Downloading of files optimized a bit!

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function copyhtml(){
 }
 
 function copytext(){
-    downtext(mdoutput.innerHTML, '.doc')
+    downtext(mdoutput, '.doc')
 }
 
 function copymarkdown(){
@@ -40,46 +40,19 @@ function copymarkdown(){
 }
 
 function downtext(text, doctype){
-    if(doctype == ".doc"){
-        var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
-        var postHtml = "</body></html>";
-        var html = preHtml+text+postHtml;
-    }
-    
-    else{
+        if(doctype == '.doc'){
+            text = new XMLSerializer().serializeToString(text)
+        }
         html = text;
-    }
-
-    var blob = new Blob(['\ufeff', html], {
-        type: 'application/msword'
-    });
-    
-    // Specify link url
-    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
-    
-    // Specify file name
-    filename = "MyMakdownToHTML"+doctype;
-    
-    // Create download link element
-    var downloadLink = document.createElement("a");
-
-    document.body.appendChild(downloadLink);
-    
-    if(navigator.msSaveOrOpenBlob ){
-        navigator.msSaveOrOpenBlob(blob, filename);
-    }else{
-        // Create a link to the file
+        var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+        filename = new XMLSerializer().serializeToString(mdoutput).slice(81,92).toUpperCase() + doctype;
+        var downloadLink = document.createElement("a");
+        document.body.appendChild(downloadLink);
         downloadLink.href = url;
-        
-        // Setting the file name
         downloadLink.download = filename;
-        
-        //triggering the function
         downloadLink.click();
-    }
-
-    document.body.removeChild(downloadLink);
-}
+        document.body.removeChild(downloadLink);
+}   
 
 function copyToClipboard(text) {
     navigator.clipboard.writeText(text).then(function() {
